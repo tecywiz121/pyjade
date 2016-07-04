13d022c9856f475c5d189328ca3217f78049552e
jmp0xf <jmp0xf@gmail.com>
2016-07-04 14:23:19 +0800

    explicitly set the app label to avoid label duplicates

--- pyjade/ext/django/__init__.py
+++ pyjade/ext/django/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
 from .compiler import Compiler
 from .loader import Loader
+
+default_app_config = 'pyjade.ext.django.apps.PyJadeConfig'
---
+++ pyjade/ext/django/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class PyJadeConfig(AppConfig):
+    name = 'pyjade.ext.django'
+    label = 'pyjade.ext.django'
